2697976df7c44850bbb7595c1fd7c33fb353d722
plow <simu@snowplow.ch>
2018-10-28 23:29:35 +0100

    Added some more descriptive output and documentation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # sync-utils
 Scripts to ensure file synchronization across multiple sources with different directory structures and file names
+
+## Base Commands
+
+Mounting the NAS volumes:
+```
+sudo su
+mount -t nfs 192.168.0.10:/volume1/photos-tmp /mnt/photos-tmp/
+```
+
+Create hashes recursively:
+```
+hashdeep -c md5 -v -r -l -W hashdeep_out.txt .
+```
+
+Auditing:
+```
+hashdeep -vvv -a -r -k /mnt/photos-tmp/2017/photos_new/hashdeep_out.txt . > hashdeep_audit.txt 2>&1
+```
+
+Regex search audit file:
+```
+/2017/.*No\smatch
+```
--- delete_existing_files.sh
+++ delete_existing_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# After an audit, this script moves all known files under the current directory to the ../removed_known directory.
+# Thus, only unknown files remain unter the current directory.
+# The removed_known directory must be created manually before running the script.
+
+echo "Number of files at the beginning:"
 find | wc -l
 
 # grep "^`pwd`"  --> only lines starting with current directory, i.e. hash lines and no comments
@@ -8,15 +13,18 @@ find | wc -l
 # xargs -d '\n' -I % mv "%" ../removed_known/  --> move all known files to folder ../removed_known
 cat hashdeep_audit.txt | grep "^`pwd`" | grep -v ": No match$" | cut -d':' -f1 | xargs -d '\n' -I % mv "%" ../removed_known/
 
+echo "Number of files remaining after separating the known files:"
 find | wc -l
 
 # delete all empty directories
 find -type d -empty -delete
 
+echo "Number of files after deleting empty directories:"
 find | wc -l
 
 
 # delete all empty files
 find . -size 0 | xargs -I % mv "%" ./size-zero/
 
+echo "Number of files after deleting all empty files:"
 find | wc -l
