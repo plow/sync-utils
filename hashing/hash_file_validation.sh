0d5bceeaac2042f9867e7e7a5b4b417501d37156
plow <simu@snowplow.ch>
2020-05-25 00:36:02 +0200

    Fixed hash file validation for paths that contain commas

--- hashing/hash_file_validation.sh
+++ hashing/hash_file_validation.sh
@@ -11,16 +11,17 @@ tmpfilelist_hashed=$(mktemp /tmp/filelist_hashed.XXXXXX)
 find . -type f | sort > $tmpfilelist_actual
 
 # list of sorted file names in hashes file
-tail -n +6 hashdeep_out.txt | awk -F '[,]' '{print $3}' | sort > $tmpfilelist_hashed
+tail -n +6 hashdeep_out.txt                     `# ommit header in hashdeep_out` \
+  | sed -e $'s/,/\t/g; s/\t/,/3g'               `# replace the first two commas (column separators) with tabs (since file names might contain commas)` \
+  | awk 'BEGIN {FS="\t"; OFS="\t"} {print $3}'  `# only use third column, i.e. file path` \
+  | sort > $tmpfilelist_hashed
 
 # compare sorted file lists with comm
 echo "| <-- Column 1: Files appearing only on file system but not in the hashes file (unique to file system)"
 echo -e "\t| <-- Column 2: Files appearing only in the hashes file but not on file system (unique to hashes file)"
 echo ""
-comm -3 $tmpfilelist_actual $tmpfilelist_hashed
+comm -3 $tmpfilelist_actual $tmpfilelist_hashed `# list lines unique to either FILE1 or FILE2. Ommit common lines (-3)`
 
 rm "$tmpfilelist_actual"
 rm "$tmpfilelist_hashed"
-
-
 
