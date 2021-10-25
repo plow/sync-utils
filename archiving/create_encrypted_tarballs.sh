cfd9584c25875106a41c7f7bedf7909ff276c675
plow <simu@snowplow.ch>
2021-10-26 00:56:33 +0200

    Better regex date matching

--- archiving/create_encrypted_tarballs.sh
+++ archiving/create_encrypted_tarballs.sh
@@ -27,10 +27,10 @@ for dir in */ ; do
   # matches "yyyy-mm-dd-some text"
   regexp2="^(19|20)[0-9]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-.*"
   if [[ $dir =~ $regexp1 ]]; then 
-    dirprefix=$(echo $dir | sed 's/^\(.*\)-.*$/\1/')
+    dirprefix=$(echo $dir | sed 's/^\([0-9]*\)-.*$/\1/')
     create
   elif [[ $dir =~ $regexp2 ]]; then
-    dirprefix=$(echo $dir | sed 's/^\(.*\)-\(.*\)-\(.*\)-.*$/\1\2\3/')
+    dirprefix=$(echo $dir | sed 's/^\([0-9]*\)-\([0-9]*\)-\([0-9]*\)-.*$/\1\2\3/')
     create
   else
     echo "WARNING: No date prefix detected for directory $dir. Prefix omitted."
@@ -38,7 +38,12 @@ for dir in */ ; do
     create
   fi
 done
+
+gpg --symmetric --cipher-algo AES256 --passphrase-file $dir_script/enc-pw.txt --batch --quiet --yes -o $dir_encmedia/hashdeep_out.txt.gpg $dir_work/hashdeep_out.txt
 gpg --symmetric --cipher-algo AES256 --passphrase-file $dir_script/enc-pw.txt --batch --quiet --yes -o $dir_encmedia/index.txt.gpg $dir_encmedia/index.txt 
 rm -f $dir_encmedia/index.txt
+
+echo "---------------------"
+echo "Output directory:  $dir_encmedia"
 
 #rm "$dir_encmedia"
